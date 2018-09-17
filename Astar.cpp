ae387c89089396ba720c308eca5f29a81f6038dd
Fernando Arriaga <a01270913@itesm.mx>
2018-09-17 08:05:46 -0500

    Update Astar.cpp

--- Astar.cpp
+++ Astar.cpp
@@ -1,407 +1,322 @@
 #include<stdio.h>
-	#include <stdlib.h>
-	#include <string>
-	#include <vector>
-	#include <iostream>
-	#include <queue>
-	#include <stack>
-	#include<list>
-	bool flag;
-	class statenode {
-	public:
-	int pathcost;
-	unsigned int Nstart;
-	unsigned int Nfinish;
-	statenode* parent;
-	std::vector<std::stack<char> > stacks;
-	void cpc(void)
-	{
-	    this->pathcost=(parent->pathcost)+1+abs(Nstart-Nfinish);
-	}
-	
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include<list>
+bool flag;
+class statenode {
+public:
+int pathcost;
+unsigned int Nstart;
+unsigned int Nfinish;
+statenode* parent;
+std::vector<std::stack<char> > stacks;
+void cpc(void)
+{
+    this->pathcost=(parent->pathcost)+1+abs(Nstart-Nfinish);
+}
 
-	friend class UCS;
-	};
-	
+friend class UCS;
+};
 
-	unsigned int hn(statenode* p, statenode* gs)
-	{
-	    int h=0;
-	    statenode current;
-	    statenode goal;
-	    std::stack<char> buff;
-	    std::vector<std::stack<char> >tempc;
-	    std::vector<std::stack<char> >tempg;
-	
+unsigned int hn(statenode current, statenode goal)
+{
+    int h=0;
+    std::stack<char> buff;
+    std::vector<std::stack<char> >tempc;
+    std::vector<std::stack<char> >tempg;
 
-	    for(unsigned int i=0; i < p->stacks.size(); i++)
-	{
-	current.stacks.push_back(p->stacks[i]);
-	}
-	 for(unsigned int i=0; i < gs->stacks.size(); i++)
-	{
-	goal.stacks.push_back(gs->stacks[i]);
-	}
-	
+ for(unsigned int i=0; i < current.stacks.size(); i++)
+{
+    while(!current.stacks[i].empty())
+    {
+buff.push(current.stacks[i].top());
+current.stacks[i].pop();
+    }
+    tempc.push_back(buff);
+    while(!buff.empty())
+        buff.pop();
+}
+for(unsigned int i=0; i < goal.stacks.size(); i++)
+{
+    while(!goal.stacks[i].empty())
+    {
+buff.push(goal.stacks[i].top());
+goal.stacks[i].pop();
+    }
+    tempg.push_back(buff);
+    while(!buff.empty())
+        buff.pop();
+}
 
-	 for(unsigned int i=0; i < current.stacks.size(); i++)
-	{
-	    while(!current.stacks[i].empty())
-	    {
-	buff.push(current.stacks[i].top());
-	current.stacks[i].pop();
-	    }
-	    tempc.push_back(buff);
-	    while(!buff.empty())
-	        buff.pop();
-	}
-	for(unsigned int i=0; i < goal.stacks.size(); i++)
-	{
-	    while(!goal.stacks[i].empty())
-	    {
-	buff.push(goal.stacks[i].top());
-	goal.stacks[i].pop();
-	    }
-	    tempg.push_back(buff);
-	    while(!buff.empty())
-	        buff.pop();
-	}
-	
+ for (unsigned int i=0; i<tempc.size();i++)
+    {
 
-	 for (unsigned int i=0; i<tempc.size();i++)
-	    {
-	
 
-	
+        while (!tempc[i].empty()&&!tempg[i].empty())
+        {
+         if(!(tempg[i].top()=='X'||tempg[i].top()=='x'))
+         {
+            if (tempc[i].top()!=tempg[i].top())
+                h+=2;
 
-	        while (!tempc[i].empty()&&!tempg[i].empty())
-	        {
-	         if(!(tempg[i].top()=='X'||tempg[i].top()=='x'))
-	         {
-	            if (tempc[i].top()!=tempg[i].top())
-	                h+=2;
-	
+            tempc[i].pop();
 
-	            tempc[i].pop();
-	
+            tempg[i].pop();
 
-	            tempg[i].pop();
-	
+        }
+        else
+            break;
+        }
+        if(!tempg[i].empty())
+        {
+        if(!(tempg[i].top()=='X'||tempg[i].top()=='x'))
+        h+=(abs(tempc[i].size()-tempg[i].size()));
+        }
+        h+=(abs(tempc[i].size()-tempg[i].size()));
+    }
 
-	        }
-	        else
-	            break;
-	        }
-	        if(!tempg[i].empty())
-	        {
-	        if(!(tempg[i].top()=='X'||tempg[i].top()=='x'))
-	        h+=(abs(tempc[i].size()-tempg[i].size()));
-	        }
-	        h+=(abs(tempc[i].size()-tempg[i].size()));
-	    }
-	
-		if(h%2!=0)
-			h+=1;
-	    return h;
-	}
-	
+    return h;
+}
 
-	
 
-	bool comp (statenode* curr, statenode* gs)
-	{
-	statenode current;
-	    statenode goal;
-	    for(unsigned int i=0; i < curr->stacks.size(); i++)
-	{
-	current.stacks.push_back(curr->stacks[i]);
-	}
-	 for(unsigned int i=0; i < gs->stacks.size(); i++)
-	{
-	goal.stacks.push_back(gs->stacks[i]);
-	}
-	
+bool comp (statenode current, statenode goal)
+{
 
-	
+    for (unsigned int i=0; i<current.stacks.size();i++)
+    {
 
-	    for (unsigned int i=0; i<current.stacks.size();i++)
-	    {
-	
 
-	
+        while (!current.stacks[i].empty()&&!goal.stacks[i].empty())
+        {
+         if(!(goal.stacks[i].top()=='X'||goal.stacks[i].top()=='x'))
+         {
+            if (current.stacks[i].top()!=goal.stacks[i].top())
+                return false;
 
-	        while (!current.stacks[i].empty()&&!goal.stacks[i].empty())
-	        {
-	         if(!(goal.stacks[i].top()=='X'||goal.stacks[i].top()=='x'))
-	         {
-	            if (current.stacks[i].top()!=goal.stacks[i].top())
-	                return false;
-	
+            current.stacks[i].pop();
 
-	            current.stacks[i].pop();
-	
+            goal.stacks[i].pop();
 
-	            goal.stacks[i].pop();
-	
+        }
+        else
+        {
 
-	        }
-	        else
-	        {
-	
+            break;
+        }
 
-	            break;
-	        }
-	
+        }
+        if((current.stacks[i].empty()!=goal.stacks[i].empty()))
+            {
+            if(!goal.stacks[i].empty())
+            {
+                if((!(goal.stacks[i].top()=='X'||goal.stacks[i].top()=='x')))
+            return false;
+            }
+            else
+                return false;
+            }
 
-	        }
-	        if((current.stacks[i].empty()!=goal.stacks[i].empty()))
-	            {
-	            if(!goal.stacks[i].empty())
-	            {
-	                if((!(goal.stacks[i].top()=='X'||goal.stacks[i].top()=='x')))
-	            return false;
-	            }
-	            else
-	                return false;
-	            }
-	
 
-	
+    }
+    return true;
+}
 
-	    }
-	    return true;
-	}
-	
 
-	
 
-	
+statenode* shift(statenode* node, int start, int finish)
+{
+statenode* next=new statenode;
+for(unsigned int i=0; i<node->stacks.size();i++)
+{
+next->stacks.push_back(node->stacks[i]);
+}
+next->stacks[finish].push(next->stacks[start].top());
+next->stacks[start].pop();
+next->parent=node;
+next->Nstart=start;
+next->Nfinish=finish;
+next->cpc();
+return next;
+}
 
-	
+class UCS {
+    public:
+unsigned int height;
+statenode* Instate;
+statenode* Goalstate;
+std::vector<statenode*> Pqueue;
+std::vector<statenode*> Visited;
 
-	
 
-	
+statenode* fn(void)
+{
+unsigned int lowestpathcost=4294967295;;
+    int j=0;
+    statenode* temp;
+    unsigned int pcost;
 
-	statenode* shift(statenode* node, int start, int finish)
-	{
-	statenode* next=new statenode;
-	for(unsigned int i=0; i<node->stacks.size();i++)
-	{
-	next->stacks.push_back(node->stacks[i]);
-	}
-	next->stacks[finish].push(next->stacks[start].top());
-	next->stacks[start].pop();
-	next->parent=node;
-	next->Nstart=start;
-	next->Nfinish=finish;
-	next->cpc();
-	return next;
-	}
-	
+ for (unsigned int i=0;i<Pqueue.size();i++)
+ {
+     pcost=(Pqueue[i]->pathcost)+hn(*Pqueue[i],*(this->Goalstate));
+     if(pcost<lowestpathcost)
+     {
+         lowestpathcost=pcost;
+         temp=Pqueue[i];
+         j=i;
+     }
+ }
+ if(Pqueue.empty())
+ {
+     flag=1;
+     return temp;
+ }
+ Pqueue.erase(Pqueue.begin()+j);
+ Visited.push_back(temp);
+ return temp;
+}
 
-	class UCS {
-	    public:
-	unsigned int height;
-	statenode* Instate;
-	statenode* Goalstate;
-	std::vector<statenode*> Pqueue;
-	std::vector<statenode*> Visited;
-	
+bool visit (statenode* p)
+{
+    for (unsigned int i =0;i < Visited.size();i++)
+    {
+        if (comp(*(Visited[i]),*p))
+        {
+            return true;}
+    }
+    return false;
+}
 
-	
+void expand (statenode* p)
+{
+    statenode* bfer;
+for(unsigned int s=0;s < p->stacks.size();s++)
+{
+    for(unsigned int f=0;f < p->stacks.size();f++)
+    {
+        if (!(p->stacks[s].empty())&&(p->stacks[f].size() < this->height)&&(f!=s)&&(s!=p->Nfinish))
+        {
 
-	statenode* fn(void)
-	{
-	unsigned int lowestpathcost=4294967295;;
-	    int j=0;
-	    statenode* temp;
-	    unsigned int pcost;
-	
+            bfer=shift(p,s,f);
+            if(!(visit(bfer)))
+            this->Pqueue.push_back(bfer);
+        }
+    }
+}
 
-	 for (unsigned int i=0;i<Pqueue.size();i++)
-	 {
-	     pcost=(Pqueue[i]->pathcost)+hn(Pqueue[i],this->Goalstate);
-	     if(pcost<lowestpathcost)
-	     {
-	         lowestpathcost=pcost;
-	         temp=Pqueue[i];
-	         j=i;
-	     }
-	 }
-	 if(Pqueue.empty())
-	 {
-	     flag=1;
-	     return temp;
-	 }
-	 Pqueue.erase(Pqueue.begin()+j);
-	 Visited.push_back(temp);
-	 return temp;
-	}
-	
+}
 
-	bool visit (statenode* p)
-	{
-	    for (unsigned int i =0;i < Visited.size();i++)
-	    {
-	        if (comp(Visited[i],p))
-	        {
-	            return true;}
-	    }
-	    return false;
-	}
-	
+friend class statenode;
+};
 
-	void expand (statenode* p)
-	{
-	    statenode* bfer;
-	for(unsigned int s=0;s < p->stacks.size();s++)
-	{
-	    for(unsigned int f=0;f < p->stacks.size();f++)
-	    {
-	        if (!(p->stacks[s].empty())&&(p->stacks[f].size() < this->height)&&(f!=s)&&(s!=p->Nfinish))
-	        {
-	
 
-	            bfer=shift(p,s,f);
-	            if(!(visit(bfer)))
-	            this->Pqueue.push_back(bfer);
-	        }
-	    }
-	}
-	
+statenode* initialize(std::string buffer, unsigned int maxs){
+statenode* temp=new statenode;
+std::stack<char> sbuffer;
+int j=0;
+for(unsigned int i=0;i<buffer.length();i++)
+{
+    if(buffer[i]>='A' && buffer[i]<='z')
+    {
+    sbuffer.push(buffer[i]);
+    }
 
-	}
-	
+    if(buffer[i]==';')
+    {
+    if(sbuffer.size()>maxs)
+    {
+        flag=1;
+        return temp;
+    }
+    temp->stacks.push_back(sbuffer);
+    j++;
+        while(!sbuffer.empty())
+        {
+        sbuffer.pop();
+        }
+    }
+}
+if(sbuffer.size()>maxs)
+    {
+        flag=1;
+        return temp;
+    }
+temp->stacks.push_back(sbuffer);
 
-	friend class statenode;
-	};
-	
+temp->parent=0;
+temp->Nstart=0;
+temp->Nfinish=0;
+temp->pathcost=0;
+return temp;
+}
 
-	
 
-	statenode* initialize(std::string buffer, unsigned int maxs){
-	statenode* temp=new statenode;
-	std::stack<char> sbuffer;
-	int j=0;
-	for(unsigned int i=0;i<buffer.length();i++)
-	{
-	    if(buffer[i]>='A' && buffer[i]<='z')
-	    {
-	    sbuffer.push(buffer[i]);
-	    }
-	
 
-	    if(buffer[i]==';')
-	    {
-	    if(sbuffer.size()>maxs)
-	    {
-	        flag=1;
-	        return temp;
-	    }
-	    temp->stacks.push_back(sbuffer);
-	    j++;
-	        while(!sbuffer.empty())
-	        {
-	        sbuffer.pop();
-	        }
-	    }
-	}
-	if(sbuffer.size()>maxs)
-	    {
-	        flag=1;
-	        return temp;
-	    }
-	temp->stacks.push_back(sbuffer);
-	
+void solution(statenode* temp)
+{
+    if (temp->parent==0)
+    {
+        std::cout<<"("<<temp->Nstart<<", "<<temp->Nfinish<<")";
+        return;
+    }
+    if (temp->parent->parent==0)
+    {
+        std::cout<<"("<<temp->Nstart<<", "<<temp->Nfinish<<")";
 
-	temp->parent=0;
-	temp->Nstart=0;
-	temp->Nfinish=0;
-	temp->pathcost=0;
-	return temp;
-	}
-	
+        return;
+    }
+    solution(temp->parent);
 
-	
+    std::cout<<"; "<<"("<<temp->Nstart<<", "<<temp->Nfinish<<")";
 
-	
+    return;
+}
 
-	void solution(statenode* temp)
-	{
-	    if (temp->parent==0)
-	    {
-	        std::cout<<"("<<temp->Nstart<<", "<<temp->Nfinish<<")";
-	        return;
-	    }
-	    if (temp->parent->parent==0)
-	    {
-	        std::cout<<"("<<temp->Nstart<<", "<<temp->Nfinish<<")";
-	
 
-	        return;
-	    }
-	    solution(temp->parent);
-	
+int main(int argc, char* argv[]) {
+std::stack<char> out;
+UCS SS;
+statenode* P;
 
-	    std::cout<<"; "<<"("<<temp->Nstart<<", "<<temp->Nfinish<<")";
-	
+std::string line;
+std::vector<std::string> lines;
+std::cin>>SS.height;
+std::getline(std::cin,line);
+scanf("%d",&SS.height);
 
-	    return;
-	}
-	
+std::getline(std::cin, line);
+SS.Instate=initialize(line,SS.height);
 
-	
+std::getline(std::cin, line);
 
-	int main(int argc, char* argv[]) {
-	std::stack<char> out;
-	UCS SS;
-	statenode* P;
-	
+SS.Goalstate=initialize(line,SS.height);
+SS.Goalstate->parent=SS.Instate;
 
-	std::string line;
-	std::vector<std::string> lines;
-	std::cin>>SS.height;
-	std::getline(std::cin,line);
-	scanf("%d",&SS.height);
-	
+if (!flag)
+{
+SS.Pqueue.push_back(SS.Instate);
+P = SS.fn();
+while(!comp(*P,*(SS.Goalstate)))
+{
+    SS.expand(P);
+    P=SS.fn();
+    if (flag)
+        break;
+}
+if (!flag)
+{
 
-	std::getline(std::cin, line);
-	SS.Instate=initialize(line,SS.height);
-	
+std::cout<<P->pathcost<<std::endl;
+solution(P);
+}
+}
+if (flag){
+    std::cout<<"No solution found"<<std::endl;
+}
 
-	std::getline(std::cin, line);
-	
 
-	SS.Goalstate=initialize(line,SS.height);
-	SS.Goalstate->parent=SS.Instate;
-	
-
-	if (!flag)
-	{
-	SS.Pqueue.push_back(SS.Instate);
-	P = SS.fn();
-	while(!comp(P,SS.Goalstate))
-	{
-	    SS.expand(P);
-	    P=SS.fn();
-	    if (flag)
-	        break;
-	}
-	if (!flag)
-	{
-	
-
-	std::cout<<P->pathcost<<std::endl;
-	solution(P);
-	}
-	}
-	if (flag){
-	    std::cout<<"No solution found"<<std::endl;
-	}
-	
-
-	
-
-	  return 0;
-	}
+  return 0;
+}
